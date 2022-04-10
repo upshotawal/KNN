65760b7511d72da134a912807829f21cab488f40
ggupshot <np03a190209@heraldcollege.edu.np>
2022-04-10 09:14:45 +0545

    css

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -114,13 +114,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jewelryshop/static')]
-# Automatically Created on Production
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jewelryshop/static')]
+# # Automatically Created on Production
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Settings for Media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

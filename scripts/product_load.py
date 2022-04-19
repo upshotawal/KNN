98d3650d479c29369564fa81dd625149073238b6
ggupshot <np03a190209@heraldcollege.edu.np>
2022-04-19 19:03:57 +0545

    eleopp

---
+++ scripts/product_load.py
@@ -0,0 +1,18 @@
+import csv  
+
+from store.models import Category, Product
+
+def run():
+    fhand = open('store/gym.csv')
+    reader = csv.reader(fhand)
+    next(reader)  
+
+    for row in reader:
+        print(row)
+
+        # create for category and recipes
+
+        c, created = Category.objects.get_or_create(title=row[7])
+
+        r = Product(sku=row[0], title=row[1], short_description=row[2], price=row[3], product_image=row[6], category=c )
+        r.save()

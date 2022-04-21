f5b57020585a20a4859c4f20c419c0944ce67e02
upshot awal <awalupshot@gmail.com>
2022-04-21 12:25:41 +0545

    cartdone

--- store/models.py
+++ store/models.py
@@ -54,7 +54,7 @@ class Product(models.Model):
     short_description = RichTextField()
     detail_description = models.TextField(
         blank=True, null=True, verbose_name="Detail Description")
-    product_image = models.FileField(
+    product_image = models.ImageField(
         upload_to='product', blank=True, null=True, verbose_name="Product Image")
     price = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(

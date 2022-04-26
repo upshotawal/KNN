a3e9396ddb7047307e930778f4efdac861f348e6
ggupshot <np03a190209@heraldcollege.edu.np>
2022-04-27 02:03:10 +0545

    newstuff

--- scripts/product_load.py
+++ scripts/product_load.py
@@ -1,5 +1,5 @@
 import csv
-from store.models import Category, Product
+from store.models import Rating, User, Product, Category
 
 
 def run():
@@ -15,3 +15,17 @@ def run():
         r = Product(sku=row[1], title=row[2],  short_description=row[3], price=row[4],
                     product_image=row[7],  category=c)
         r.save()
+
+# def run():
+#     fhand = open('store/users.csv')
+#     reader = csv.reader(fhand)
+#     next(reader)
+
+#     for row in reader:
+#         print(row)
+
+#         u, created = User.objects.get_or_create(title=row[2])
+#         p, created = Product.objects.get_or_create(title=row[1])
+
+#         r = Rating(rate=row[3], user_id=u, product_id=p)
+#         r.save()
